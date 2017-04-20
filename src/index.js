146828b86c41341c0cc82caa6430e4e0a14ea027
HakurouKen <ticshot@gmail.com>
2017-04-20 20:48:24 +0800

    Add progress bar.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import path from 'path'
 import _ from 'lodash'
 import {SqlSaver as Saver} from './utils/saver'
 import DB from '../db.privacy'
+import ProgressBar from 'progress'
 
 const files = fs.readdirSync(path.join(__dirname, './sites'));
 
@@ -19,12 +20,21 @@ const files = fs.readdirSync(path.join(__dirname, './sites'));
         collate: 'utf8_general_ci'
       }
     })
+    console.log('Syncing database...')
     await saver.sync()
+    console.log('Start download...')
+    let bar = new ProgressBar('[:bar] :percent Site :site', {
+      total: SiteConstructors.length
+    })
     for (let Site of SiteConstructors) {
       const site = new Site()
       const data = await site.get()
+      bar.tick({
+        site: Site.name
+      })
       await saver.append(data)
     }
+    console.log('Download finish.')
   } catch (e) {
     console.log(e, e.stack)
   }
